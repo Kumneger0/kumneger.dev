06211eb83832e1e7fefa6faabef9335436b5cb99
kumneger0 <kumnegerwondimu01@gmail.com>
2025-11-19 22:44:55 +0300

    add new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,12 +50,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server",
+  output: "static",
   adapter: vercel({ 
-    
     webAnalytics: {
       enabled: true,
-
     },
   }),
   server: {
